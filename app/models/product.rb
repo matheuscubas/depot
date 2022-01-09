ba5544efef98559bcf1af19ebccd356a828a626b
Ryuukhi <maatheusviidal@hotmail.com>
2022-01-09 18:20:18 -0300

    added baisc layout and test for controllers

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ApplicationRecord
-  validates :title, :description, :image_url, presence: true
+  validates :title, presence: true, length: { minimum: 10 }
+  validates :description, :image_url, presence: true
   validates :price, numericality: { greather_than_or_equal_to: 0.01 }
   validates :image_url, allow_blank: true, format: { with: %r{\.(gif|jpg|png)\z}i, message: 'must be a URL for GIF, JPG or PNG image.'  }
 end
