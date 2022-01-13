3b4f31bc18b7be4afe6016e5cac4e4951955399b
Ryuukhi <maatheusviidal@hotmail.com>
2022-01-13 20:34:36 -0300

    updated basic layout and added partials to remove logic from view

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,7 +1,9 @@
 class StoreController < ApplicationController
+  include CurrentCart
   before_action :initialize_visit_counter
   before_action :increment_visit_counter
   before_action :get_time
+  before_action :set_cart
 
   def index
     @products = Product.order(:title)
