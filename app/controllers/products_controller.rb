54d40037422934ab2f5118aa0935ec31831d1000
Ryuukhi <maatheusviidal@hotmail.com>
2022-01-15 18:51:01 -0300

    added ActionCable broadcast to update the catalog

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,12 @@
 class ProductsController < ApplicationController
   before_action :set_product, only: %i[ show edit update destroy ]
   before_action :get_time
+  before_action :initialize_visit_counter
+  before_action :increment_visit_counter
 
   # GET /products or /products.json
   def index
-    @products = Product.all
+    @products = Product.all.order(:title)
   end
 
   # GET /products/1 or /products/1.json
@@ -42,6 +44,12 @@ class ProductsController < ApplicationController
       if @product.update(product_params)
         format.html { redirect_to product_url(@product), notice: "Product was successfully updated." }
         format.json { render :show, status: :ok, location: @product }
+
+        @products = Product.all.order(:title)
+        ActionCable.server.broadcast(
+          'products',
+          {html: render_to_string('store/index', layout: false), p_id: @product.id},
+        )
       else
         format.html { render :edit, status: :unprocessable_entity }
         format.json { render json: @product.errors, status: :unprocessable_entity }
