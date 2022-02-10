0cd6d7a97c4bda5e8af4dd796acc0ef89fd04289
Ryuukhi <maatheusviidal@hotmail.com>
2022-02-10 12:20:06 -0300

    added basic translation support using i18n

--- config/routes.rb
+++ config/routes.rb
@@ -7,12 +7,16 @@ Rails.application.routes.draw do
   end
   get 'sessions/create'
   get 'sessions/destroy'
+  
   resources :users
-  resources :orders
-  resources :line_items
-  resources :carts
-  root 'store#index', as: 'store_index'
   resources :products do
     get :who_bought, on: :member
+  end
+
+  scope '(:locale)' do
+    resources :orders
+    resources :line_items
+    resources :carts
+    root 'store#index', as: 'store_index'
   end
 end
